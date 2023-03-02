c94ff1374f44e40e1cd469f1535384a99b631dee
Roshantiwary1 <tiwariroshan9999@gmail.com>
2023-03-02 11:18:01 +0530

    post method using express.js

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simpl
 //JSon.parse is to convert json into javascript object
 //json.stingify is to convert javascript object to json format
 
+app.use(express.json());
+
 app.get("/api/v1/tours",(req,res)=>{
     res.status(200).json({
         status:"success",
@@ -14,6 +16,27 @@ app.get("/api/v1/tours",(req,res)=>{
             tours,
         }
     })
+})
+
+app.post("/api/v1/tours",(req,res)=>{
+    // console.log(req.body);
+    const newId = tours[tours.length-1].id + 1;
+    const newTour = Object.assign({id:newId} , req.body) 
+    //Object.assign add new object newTour to existing object tours 
+
+    tours.push(newTour);
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours),err=>{
+        res.status(201).json({
+            status:"success",
+            data:{
+                tour:newTour
+            }
+        })
+    })
+
+
+    res.send("done")
 })
 
 app.listen(3000,()=>{
